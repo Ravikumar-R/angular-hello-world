09f5a1700269f6a0840a907cec0e5eda62004079
Ravikumar R <ravikumar.rangasamy@klm.com>
2019-12-11 22:21:41 +0100

    Angular Basic stuffs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  messageFromParent = 'We name you good';
+  languages = ['Tamil', 'English'];
   title = 'hello-world';
+  name = 'Ravi';
+  country = 'enter country';
+  state = 'enter state';
+  city = 'enter city';
+  DOJ = new Date();
+  disableDOJ = true;
+  displaySmallText = true;
+  warning = '';
+  comments = '';
+  commentsThere = false;
+  conditionClass = {
+    'text-small': this.displaySmallText,
+    'text-large': !this.displaySmallText,
+    'text-italic': true
+  };
+  styleClass = {
+    fontStyle: 'italic'
+  }
+  callCountry(event) {
+    console.log('event received');
+    this.country = 'india';
+  }
+  checkComments() {
+    if (this.comments.length > 0) {
+      this.commentsThere = true;
+    } else {
+      this.commentsThere = false;
+    }
+  }
+  validate(value) {
+    this.warning = value + ' is not valid';
+  }
 }
